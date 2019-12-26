5b3f4d5715c4b3f9d642ceb58fcb97179be37b19
David Pätzel <david.paetzel@informatik.uni-augsburg.de>
2019-12-26 07:03:23 +0100

    Rework listing locally stored transactions, add Haddock comments

--- src/Buchhaltung/Aqbanking/Request.hs
+++ src/Buchhaltung/Aqbanking/Request.hs
@@ -18,15 +18,56 @@ import Buchhaltung.Config
 import Data.Text as T
 import Protolude
 
+{-|
+From @aqbanking-cli listtrans --help@:
+
+> Default is (all in one line):
+>   $(dateOrValutaDateAsString)\t$(valueAsString)\t$(localBankcode)\t
+>    $(localAccountNumber)\t$(localIban)\t$(remoteName)\t$(remoteIban)\t
+>    $(purposeInOneLine)
+-}
 localTransactions :: Aq Text
 localTransactions = do
   conf <- ask
   read aqbankingExe
-    [ "export",
-      "-c",
-      contextFile conf
+    [ "listtrans",
+      "--ctxfile=" <> contextFile conf,
+      "--template=" <> "$(dateOrValutaDateAsString)\t$(localIban)\t$(remoteIban)\t$(valueAsString)\t$(purposeInOneLine)"
     ]
 
+{-|
+Uses the @export@ subcommand to list local transactions.
+
+This seems to work not as stably as the @listtrans@ subcommand used by
+'localTransaction' in that the account numbers are sometimes lacking (e.g. for
+Kreissparkasse connections)?
+
+  1="transactionId"
+    2="localBankCode"
+    3="localAccountNumber"
+    4="remoteBankCode"
+    5="remoteAccountNumber"
+    6="date"
+    7="valutadate"
+    8="value/value"
+    9="value/currency"
+    10="localName"
+    11="remoteName[0]"
+    12="remoteName[1]"
+
+-}
+localTransactions' :: Aq Text
+localTransactions' = do
+  conf <- ask
+  read aqbankingExe
+    [ "export",
+      "--ctxfile=" <> contextFile conf
+    ]
+
+-- TODO Find out whether dates are needed here
+{-|
+Uses the @request@ subcommand to retrieve transactions.
+-}
 getTransactions :: Aq ()
 getTransactions = do
   conf <- ask
@@ -34,6 +75,5 @@ getTransactions = do
     [ "request",
       "--transactions",
       "--ignoreUnsupported",
-      "-c",
-      contextFile conf
+      "-ctxfile=" <> contextFile conf
     ]
