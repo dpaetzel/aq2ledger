cda87dbcc7e3420d807ded669648ebf71581a335
David Pätzel <david.paetzel@informatik.uni-augsburg.de>
2019-12-29 18:17:05 +0100

    Update default.nix and Cabal description
    … which I plainly forgot to add to VC earlier.

--- default.nix
+++ default.nix
@@ -1,11 +1,39 @@
-{ mkDerivation, base, protolude, stdenv }:
+{ mkDerivation, aeson, base, bytestring, directory, filepath, hledger-lib, mtl
+, process, protolude, QuickCheck, stdenv, text, aqbanking }:
 mkDerivation {
   pname = "buchhaltung2";
   version = "0.1.0.0";
   src = ./.;
-  isLibrary = false;
+  isLibrary = true;
   isExecutable = true;
-  executableHaskellDepends = [ base protolude ];
+  libraryHaskellDepends = [
+    aeson
+    base
+    bytestring
+    directory
+    filepath
+    hledger-lib
+    mtl
+    process
+    protolude
+    QuickCheck
+    text
+  ];
+  executableHaskellDepends = [
+    aeson
+    base
+    bytestring
+    directory
+    filepath
+    hledger-lib
+    mtl
+    process
+    protolude
+    QuickCheck
+    text
+  ];
+  executableSystemDepends = [ aqbanking ];
+  doHaddock = false;
   homepage = "https://github.com/dpaetzel/buchhaltung2";
   description = "Automates plain text accounting data entry in ledger format";
   license = stdenv.lib.licenses.gpl3;
